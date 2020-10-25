a2b9fcf56e7c14f2905a2b3061efe1e9cfe48a86
Byungchan-Park <pbc9236@gmail.com>
2020-10-25 21:51:07 +0900

    Update: Process the response data depending on limit value

--- app.js
+++ app.js
@@ -13,8 +13,12 @@ app.get("/", (req, res) => res.send("Hello World!"))
 // res.send는 문자열을 인자로 가지므로, 객체를 클라이언트에게 json 형식으로 전달할 때는 res.json 함수를 사용한다.
 app.get("/users", (req, res) => {
   // req.query : 이를 이용하여 query string에 접근할 수 있다.
-  console.log(req.query)
-  res.json(users)
+  // limit을 요청하지 않으면 기본값은 10이다.
+  req.query.limit = req.query.limit || 10
+  // limit을 10진수의 숫자로 변경한다.
+  const limit = parseInt(req.query.limit, 10)
+  // limit 갯수만큼만 응답한다. 
+  res.json(users.slice(0, limit))
 })
 
 // 어플리케이션을 외부에서도 사용할 수 있도록 모듈로 만들어준다.
