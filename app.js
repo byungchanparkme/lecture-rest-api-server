545ef22a7bf05f21735e41e1c19b223c5ba08ed4
Byungchan-Park <pbc9236@gmail.com>
2020-10-25 18:21:14 +0900

    Update: Load third-party middleware named morgan and execute

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
 const express = require("express")
 // 요청에 대한 로깅 메세지를 보여준다.
-
+const logger = require("morgan")
 const app = express()
 
-// 로깅하기 위한 미들웨어 정의
-const mw = (req, res, next) => {
-  console.log("mw")
-  // next를 반드시 호출해야 그 다음 어플리케이션의 행동을 수행하게 할 수 있다.
-  next()
-}
-const mw2 = (req, res, next) => {
-  console.log("mw2")
-  next()
-}
-
-app.use(mw)
-app.use(mw2)
+app.use(logger("dev"))
 
 app.listen(3000, () => console.log("running"))
