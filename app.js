d415581a395f0dd991f561ea1a16827740003766
Byungchan-Park <pbc9236@gmail.com>
2020-10-25 21:30:02 +0900

    Update: Test api with supertest

--- app.js
+++ app.js
@@ -8,4 +8,7 @@ app.get("/", (req, res) => res.send("Hello World!"))
 // res.send는 문자열을 인자로 가지므로, 객체를 클라이언트에게 json 형식으로 전달할 때는 res.json 함수를 사용한다.
 app.get("/users", (req, res) => res.json(users))
 
-app.listen(3000, () => console.log("running"))
+// app.listen(3000, () => console.log("running"))
+
+// 어플리케이션을 외부에서도 사용할 수 있도록 모듈로 만들어준다.
+module.exports = app
--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,21 @@
 const assert = require("assert")
+const should = require("should")
+const request = require("supertest")
+const app = require("./app")
 
 describe("GET /users", () => {
-  it("배열을 반환한다", () => {
-    assert.equal(0, 0)
+  it("배열을 반환한다", (done) => {
+    request(app)
+      .get("/users")
+      .end((err, res) => {
+        if (err) throw err
+        // 응답값이 배열이어야 한다.
+        res.body.should.be.instanceof(Array)
+        // 배열 안의 요소들은 name이라는 key를 가지고 있어야 한다.
+        res.body.forEach((user) => {
+          user.should.have.property("name")
+        })
+        done()
+      })
   })
 })
