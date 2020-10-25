dc91232b2ad0aec3931f26da9fdf8255dc7b28c4
Byungchan-Park <pbc9236@gmail.com>
2020-10-25 21:45:02 +0900

    Update: Add testing code about limit without changing api logic

--- index.spec.js
+++ index.spec.js
@@ -4,6 +4,10 @@ const request = require("supertest")
 const app = require("./app")
 
 describe("GET /users", () => {
+  // 첫번째 인자에는 test spec 작성
+  // 두번째 인자의 콜백 함수에 들어가는 done이라는 인자는,
+  // 응답 객체의 검증이 완료된 후에 완료 여부를 mocha에게 알려준다.
+  // 서버를 구동시키고 요청을 보내고 응답을 받는 과정은 모두 비동기 작업이다.
   it("배열을 반환한다", (done) => {
     request(app)
       .get("/users")
@@ -15,6 +19,16 @@ describe("GET /users", () => {
         res.body.forEach((user) => {
           user.should.have.property("name")
         })
+        done()
+      })
+  })
+  it("최대 limit 갯수만큼 응답한다.", (done) => {
+    request(app)
+      .get("/users?limit=2")
+      .end((err, res) => {
+        if (err) throw err
+        // 응답값의 길이는 2여야 한다.
+        res.body.should.have.lengthOf(2)
         done()
       })
   })
