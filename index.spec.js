3248b7c9384784d669de942292fcd346f9514e86
Byungchan-Park <pbc9236@gmail.com>
2020-10-25 21:57:28 +0900

    Update: Separate testing codes on each condition and add the failure case
    <description>
    if limit value is not a number, then it is failure case and send 400 status code.

--- index.spec.js
+++ index.spec.js
@@ -4,32 +4,41 @@ const request = require("supertest")
 const app = require("./app")
 
 describe("GET /users", () => {
-  // 첫번째 인자에는 test spec 작성
-  // 두번째 인자의 콜백 함수에 들어가는 done이라는 인자는,
-  // 응답 객체의 검증이 완료된 후에 완료 여부를 mocha에게 알려준다.
-  // 서버를 구동시키고 요청을 보내고 응답을 받는 과정은 모두 비동기 작업이다.
-  it("배열을 반환한다", (done) => {
-    request(app)
-      .get("/users")
-      .end((err, res) => {
-        if (err) throw err
-        // 응답값이 배열이어야 한다.
-        res.body.should.be.instanceof(Array)
-        // 배열 안의 요소들은 name이라는 key를 가지고 있어야 한다.
-        res.body.forEach((user) => {
-          user.should.have.property("name")
+  // 성공하는 경우와 실패하는 경우를 나눠준다.
+  describe("성공", () => {
+    it("배열을 반환한다", (done) => {
+      request(app)
+        .get("/users")
+        .end((err, res) => {
+          if (err) throw err
+          // 응답값이 배열이어야 한다.
+          res.body.should.be.instanceof(Array)
+          // 배열 안의 요소들은 name이라는 key를 가지고 있어야 한다.
+          res.body.forEach((user) => {
+            user.should.have.property("name")
+          })
+          done()
         })
-        done()
-      })
+    })
+    it("최대 limit 갯수만큼 응답한다.", (done) => {
+      request(app)
+        .get("/users?limit=2")
+        .end((err, res) => {
+          if (err) throw err
+          // 응답값의 길이는 2여야 한다.
+          res.body.should.have.lengthOf(2)
+          done()
+        })
+    })
   })
-  it("최대 limit 갯수만큼 응답한다.", (done) => {
-    request(app)
-      .get("/users?limit=2")
-      .end((err, res) => {
-        if (err) throw err
-        // 응답값의 길이는 2여야 한다.
-        res.body.should.have.lengthOf(2)
-        done()
-      })
+  describe("실패", () => {
+    it("limit이 정수가 아니면 400을 응답한다", (done) => {
+      request(app)
+        .get("/users?limit=two")
+        // 상태값만 따로 체크해준다.
+        // limit이 숫자가 아니기 때문에 status code는 400이 와야 한다. 그것으로 테스트는 끝난다.
+        .expect(400)
+        .end(done)
+    })
   })
 })
