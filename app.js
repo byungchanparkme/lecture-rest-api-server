f03ec7bf7356de68b89e187037debb412d88ce16
Byungchan-Park <pbc9236@gmail.com>
2020-10-25 18:42:46 +0900

    Update: Make routes of the server

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
 const express = require("express")
-// 요청에 대한 로깅 메세지를 보여준다.
-const logger = require("morgan")
 const app = express()
+const users = [{ name: "Ron" }, { name: "Alice" }]
 
-const mw = (req, res, next) => {
-  // throw Error("error!")
-  next()
-}
-// 에러 미들웨어는 이전의 미들웨어들이 호출되는 과정 중에 에러가 발생하지 않는다면 호출되지 않는다.
-// 오로지 에러가 발생할 때만 이를 감지한다.
-const errorMw = (err, req, res, next) => {
-  console.log(err.message)
-}
-
-app.use(logger("dev"))
-app.use(mw)
-app.use(errorMw)
+// 클라이언트로부터 root 경로로 요청이 들어오면 두 번째 인자로 받은 콜백 함수를 실행한다.
+// res.send는 res.status(), res.setHeader(), res.end()가 했던 동작을 단 하나의 함수로 가능하게 해준다.
+app.get("/", (req, res) => res.send("Hello World!"))
+// res.send는 문자열을 인자로 가지므로, 객체를 클라이언트에게 json 형식으로 전달할 때는 res.json 함수를 사용한다.
+app.get("/users", (req, res) => res.json(users))
 
 app.listen(3000, () => console.log("running"))
