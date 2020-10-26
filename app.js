1c177d4ff1ea0b6db5d12df71be1dfdecc779dd0
Byungchan-Park <pbc9236@gmail.com>
2020-10-26 15:23:00 +0900

    Update: Add GET user api

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const users = [
 // res.send는 res.status(), res.setHeader(), res.end()가 했던 동작을 단 하나의 함수로 가능하게 해준다.
 app.get("/", (req, res) => res.send("Hello World!"))
 // res.send는 문자열을 인자로 가지므로, 객체를 클라이언트에게 json 형식으로 전달할 때는 res.json 함수를 사용한다.
+
+// 유저 목록을 조회하는 API
 app.get("/users", (req, res) => {
   // req.query : 이를 이용하여 query string에 접근할 수 있다.
   // limit을 요청하지 않으면 기본값은 10이다.
@@ -23,6 +25,25 @@ app.get("/users", (req, res) => {
   } else {
     // limit 갯수만큼만 응답한다.
     res.json(users.slice(0, limit))
+  }
+})
+
+// 유저를 조회하는 API
+app.get("/users/:id", (req, res) => {
+  // id 값을 얻어낸다.
+  const id = parseInt(req.params.id, 10)
+  // id가 숫자가 아닐 경우
+  if (Number.isNaN(id)) {
+    return res.status(400).end()
+  } else {
+    // users 배열 조회
+    const currentUser = users.find((user) => user.id === id)
+    // id로 유저를 찾을 수 없는 경우
+    if (!currentUser) {
+      return res.status(404).end()
+    }
+    // 유저가 존재하면, 유저 데이터를 넣어 응답한다.
+    res.json(currentUser)
   }
 })
 
